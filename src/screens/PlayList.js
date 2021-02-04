644ff59a4f1feb9feba74b56c4769135850beebd
T3sultan <incognito.soul011@gmail.com>
2021-02-05 03:04:35 +0600

    Music App with expo react native

---
+++ src/screens/PlayList.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const PlayList =()=>{
+    return(
+        <View style={styles.container}>
+        <Text>Play List</Text>
+
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+  container: {
+      flex:1,
+      justifyContent:"center",
+      alignItems:"center",
+
+    
+  },
+});
+export default PlayList;
