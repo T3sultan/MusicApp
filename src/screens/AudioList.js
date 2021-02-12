119dc534fc6d86ad342a2d2635981ffb9720f5b4
T3sultan <incognito.soul011@gmail.com>
2021-02-13 04:26:56 +0600

    Rendering Audio Files

--- src/screens/AudioList.js
+++ src/screens/AudioList.js
@@ -1,24 +1,53 @@
 
-import React, { useEffect,Component } from 'react';
-import { StyleSheet, Text, View,ScrollView} from 'react-native';
+import React, { useEffect, Component } from 'react';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
 import { AudioContext } from '../context/AudioProvider';
 //import*as MediaLibrary from 'expo-media-library';
+import { RecyclerListView, LayoutProvider } from 'recyclerlistview'
 
-export class AudioList extends Component{
-    static contextType = AudioContext
-    render(){
+export class AudioList extends Component {
+    static contextType = AudioContext;
+
+    layoutProvider = new LayoutProvider((i) => 'audio', (type, dim) => {
+        switch (type) {
+            case "audio":
+                dim.width = Dimensions.get('window').width;
+                dim.height = 70;
+                break;
+            default:
+                dim.width = 0;
+                dim.height = 0;
+
+        }
+
+    })
+    rowRenderer = (type, item) => {
+        return <Text>{item.filename}</Text>
+
+    }
+
+    render() {
         return (
-            <ScrollView>
-            {this.context.audioFiles.map(item=> <Text
-            style={{padding:10,borderBottomColor:'red',borderBottomWidth:2}}
-             key={item.id}>{item.filename}</Text>)}
-               
-    
-            </ScrollView>
+            <AudioContext.Consumer>
+                {({ dataProvider }) => {
+                    return (
+                        <View style={{flex:1}}>
+                        <RecyclerListView
+                            dataProvider={dataProvider}
+                            layoutProvider={this.layoutProvider}
+                            rowRenderer={this.rowRenderer}
+                        />
+
+                    </View>
+                    )
+
+
+                }}
+            </AudioContext.Consumer>
         )
 
     }
-    
+
 }
 
 const styles = StyleSheet.create({
